27d439f0c7df03226ab2b6786b86f3e6d1d9bf4b
Roman Varshavski <raman.varshavskiy@gmail.com>
2025-09-21 11:26:34 +0200

    create base.html, products_list.html, product_detail.html. Update views.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home
+from catalog.views import contacts, home, product_detail, products_list
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
+    path("catalog/", home, name="home"),
+    path("catalog/contacts/", contacts, name="contacts"),
+    path("catalog/products_list/", products_list, name="products_list"),
+    path("catalog/product_detail/<int:product_id>", product_detail, name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -17,10 +17,21 @@ def home(request):
         print(f"- [{p.id}] {p.name} | создан: {p.created_at} | цена: {p.price}")
 
     # Передаём оба queryset'а в шаблон
-    return render(request, "home.html", {
+    return render(request, "catalog/home.html", {
         "products": products,
         "last_products": last_products,
     })
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/products_list.html', context)
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
 
 
 def contacts(request):
@@ -30,5 +41,5 @@ def contacts(request):
         message = request.POST.get("message")
         return HttpResponse(f"Спасибо {name}. Сообщение успешно отправлено")
     contacts = Contact.objects.all()  # получаем контакты из БД
-    return render(request, "contacts.html", {"contacts": contacts})
+    return render(request, "catalog/contacts.html", {"contacts": contacts})
 
